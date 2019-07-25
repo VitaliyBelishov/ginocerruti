6a67a357f644b8a88aab1243a490954cf949f5b6
Vitaliy Belishov <belishov.v@gmail.com>
2019-07-25 20:09:51 +0300

    hooks

--- source/size/size-guide.screen.js
+++ source/size/size-guide.screen.js
@@ -1,8 +1,7 @@
-import * as React from 'react';
+import React, { useState, Fragment, useRef } from 'react';
 import DeviceInfo from 'react-native-device-info';
 import {
   SafeAreaView,
-  ScrollView,
   View,
   Text,
   StatusBar,
@@ -26,196 +25,194 @@ const TitleText = props => (
   </View>
 );
 
-class SizeGuide extends React.Component {
-  state = {
-    sizeOption: 'sm',
-    bust: '',
-    weist: '',
-    hips: '',
-    calculateSize: '',
-  };
+const SizeGuide = () => {
+  const [sizeOption, setSizeOption] = useState('sm');
+  const [calculateSize, setCalculateSize] = useState('');
+  const [bust, setBust] = useState('');
+  const [weist, setWeist] = useState('');
+  const [hips, setHips] = useState('');
+  const bustRef = useRef(null);
+  const weistRef = useRef(null);
+  const hipsRef = useRef(null);
 
-  onChangeSizeOption = option => this.setState({ sizeOption: option, calculateSize: '' });
-
-  onChangeText = (text, key) => this.setState({ [key]: text, calculateSize: '' });
-
-  convertSmToInches = value => {
-    if (this.state.sizeOption === 'sm') return value;
+  const convertSmToInches = value => {
+    if (sizeOption === 'sm') return value;
     return (value / 2.54).toFixed(2);
   }
 
-  calculate = () => {
+  const calculate = () => {
     Keyboard.dismiss();
-    const bust = parseFloat(this.state.bust);
-    const weist = parseFloat(this.state.weist);
-    const hips = parseFloat(this.state.hips);
+    const bustFloat = parseFloat(bust);
+    const weistFloat = parseFloat(weist);
+    const hipsFloat = parseFloat(hips);
     let maxSize = '';
     sizesList.map(size => {
-      if (bust >= this.convertSmToInches(sizes.bust.gino[size])) {
+      if (bustFloat >= convertSmToInches(sizes.bust.gino[size])) {
         maxSize = size;
       }
-      if (weist >= this.convertSmToInches(sizes.weist.gino[size])) {
+      if (weistFloat >= convertSmToInches(sizes.weist.gino[size])) {
         maxSize = size;
       }
-      if (hips >= this.convertSmToInches(sizes.hips.gino[size])) {
+      if (hipsFloat >= convertSmToInches(sizes.hips.gino[size])) {
         maxSize = size;
       }
       return size;
     });
-    if (maxSize) this.setState({ calculateSize: maxSize });
+    if (maxSize) setCalculateSize(maxSize);
   };
 
-  _focusNextField(nextField) {
-    if (this.refs[nextField] && this.refs[nextField].refs && this.refs[nextField].refs[nextField]) {
-      this.refs[nextField].refs[nextField].focus();
+  const _focusNextField = (nextField, nameField) => {
+    if (
+      nextField &&
+      nextField.current &&
+      nextField.current.refs &&
+      nextField.current.refs[nameField]
+    ) {
+      nextField.current.refs[nameField].focus();
     }
   }
 
 
-  render() {
-    const { sizeOption, bust, weist, hips, calculateSize } = this.state;
-    console.log('sizeOption =>', sizeOption);
-    return (
-      <React.Fragment>
-        <StatusBar barStyle="dark-content" />
-        <SafeAreaView>
-          <KeyboardAwareScrollView
-            contentInsetAdjustmentBehavior="automatic"
-            style={styles.scrollView}
-            keyboardShouldPersistTaps="handled"
-            showsVerticalScrollIndicator={false}
-            enableOnAndroid
-          >
-            <React.Fragment>
-              <TitleText text="Whats my size ?" />
-              <View style={styles.collectionsContainter}>
-                <Text style={styles.collectionsText}>
-                  Please select collection:
+  return (
+    <Fragment>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <KeyboardAwareScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}
+          keyboardShouldPersistTaps="handled"
+          showsVerticalScrollIndicator={false}
+          enableOnAndroid
+        >
+          <Fragment>
+            <TitleText text="Whats my size ?" />
+            <View style={styles.collectionsContainter}>
+              <Text style={styles.collectionsText}>
+                Please select collection:
+              </Text>
+              <Button
+                whiteBordered
+                style={styles.button}
+                onPress={() => {}}
+              >
+                <Text style={styles.buttonText}>
+                  Evening Wear
                 </Text>
-                <Button
-                  whiteBordered
-                  style={styles.button}
-                  onPress={() => {}}
-                >
-                  <Text style={styles.buttonText}>
-                    Evening Wear
-                  </Text>
-                </Button>
-                <Image
-                  resizeMode="contain"
-                  style={styles.image}
-                  source={{ uri: 'https://ginocerruti.com/img/gc-size-guide.jpg' }}
+              </Button>
+              <Image
+                resizeMode="contain"
+                style={styles.image}
+                source={{ uri: 'https://ginocerruti.com/img/gc-size-guide.jpg' }}
+              />
+            </View>
+            <TitleText text="Size calculator" />
+            <View style={styles.calculateContainer}>
+              <Text style={styles.calculateText}>
+                Using size chart: Bridesmaids, Evening Wear, Prom Dresses,Lenovia Bridal ,LENOVIA VIP
+              </Text>
+              <View style={styles.switchContainer}>
+                <Text style={styles.marginRight}>
+                  cm
+                </Text>
+                <Switch
+                  value={sizeOption === 'inches'}
+                  style={styles.marginRight}
+                  trackColor={{
+                    false: mixins.color.green,
+                    true: mixins.color.green,
+                  }}
+                  thumbColor={IS_IOS ? null : mixins.color.white}
+                  ios_backgroundColor={mixins.color.green}
+                  onValueChange={res => setSizeOption(res ? 'inches' : 'sm')}
                 />
-              </View>
-              <TitleText text="Size calculator" />
-              <View style={styles.calculateContainer}>
-                <Text style={styles.calculateText}>
-                  Using size chart: Bridesmaids, Evening Wear, Prom Dresses,Lenovia Bridal ,LENOVIA VIP
+                <Text style={styles.marginRight}>
+                  inches
                 </Text>
-                <View style={styles.switchContainer}>
-                  <Text style={styles.marginRight}>
-                    cm
-                  </Text>
-                  <Switch
-                    value={sizeOption === 'inches'}
-                    style={styles.marginRight}
-                    trackColor={{
-                      false: mixins.color.green,
-                      true: mixins.color.green,
-                    }}
-                    thumbColor={IS_IOS ? null : mixins.color.white}
-                    ios_backgroundColor={mixins.color.green}
-                    onValueChange={res => this.onChangeSizeOption(res ? 'inches' : 'sm')}
-                  />
-                  <Text style={styles.marginRight}>
-                    inches
-                  </Text>
-                </View>
-                <View style={styles.inputContainer}>
-                  <View style={styles.inputWrap}>
-                    <Text style={styles.swithText}>
-                      Bust
-                    </Text>
-                    <InputText
-                      styleInputInner={styles.input}
-                      placeholder="Bust"
-                      value={bust}
-                      onChange={text => this.onChangeText(text, 'bust')}
-                      keyboardType="numeric"
-                      returnKeyType={IS_IOS ? 'done' : 'next'}
-                      ref="bust"
-                      name="bust"
-                      {...(IS_IOS ? {} : { onSubmitEditing: () => this._focusNextField('weist')})}
-                    />
-                  </View>
-                  <View style={styles.inputWrapMiddle}>
-                    <Text style={styles.swithText}>
-                      Weist
-                    </Text>
-                    <InputText
-                      styleInputInner={styles.input}
-                      placeholder="Weist"
-                      value={weist}
-                      onChange={text => this.onChangeText(text, 'weist')}
-                      keyboardType="numeric"
-                      returnKeyType={IS_IOS ? 'done' : 'next'}
-                      ref="weist"
-                      name="weist"
-                      {...(IS_IOS ? {} : { onSubmitEditing: () => this._focusNextField('hips')})}
-                    />
-                  </View>
-                  <View style={styles.inputWrap}>
-                    <Text style={styles.swithText}>
-                      Hips
-                    </Text>
-                    <InputText
-                      styleInputInner={styles.input}
-                      placeholder="Hips"
-                      value={hips}
-                      onChange={text => this.onChangeText(text, 'hips')}
-                      returnKeyType="done"
-                      keyboardType="numeric"
-                      ref="hips"
-                      name="hips"
-                      onSubmitEditing={this.calculate}
-                    />
-                  </View>
-                </View>
-                {
-                  calculateSize ? (
-                    <View style={{ flex: 1 }}>
-                      <Text style={styles.calculateText}>
-                        Your size is:
-                        <Text style={styles.calculateResultTextBold}>
-                          {` ${calculateSize}`}
-                        </Text>
-                      </Text>
-                      <Text style={styles.calculateText}>
-                        Your UK Size is:
-                        <Text style={styles.calculateResultTextBold}>
-                          {` ${sizes.UK.gino[calculateSize]}`}
-                        </Text>
-                      </Text>
-                    </View>
-                  ) : null
-                }
-                <Button
-                  style={styles.buttonCal}
-                  success
-                  onPress={this.calculate}
-                >
-                  <Text style={styles.buttonTextCal}>
-                    Calculate
-                  </Text>
-                </Button>
-                <TitleText text="Size Charts" />
               </View>
-            </React.Fragment>
-          </KeyboardAwareScrollView>
-        </SafeAreaView>
-      </React.Fragment>
-    );
-  }
+              <View style={styles.inputContainer}>
+                <View style={styles.inputWrap}>
+                  <Text style={styles.swithText}>
+                    Bust
+                  </Text>
+                  <InputText
+                    styleInputInner={styles.input}
+                    placeholder="Bust"
+                    value={bust}
+                    onChange={text => setBust(text)}
+                    keyboardType="numeric"
+                    returnKeyType={IS_IOS ? 'done' : 'next'}
+                    ref={bustRef}
+                    name="bustRef"
+                    {...(IS_IOS ? {} : { onSubmitEditing: () => _focusNextField(weistRef, 'weistRef')})}
+                  />
+                </View>
+                <View style={styles.inputWrapMiddle}>
+                  <Text style={styles.swithText}>
+                    Weist
+                  </Text>
+                  <InputText
+                    styleInputInner={styles.input}
+                    placeholder="Weist"
+                    value={weist}
+                    onChange={text => setWeist(text)}
+                    keyboardType="numeric"
+                    returnKeyType={IS_IOS ? 'done' : 'next'}
+                    ref={weistRef}
+                    name="weistRef"
+                    {...(IS_IOS ? {} : { onSubmitEditing: () => _focusNextField(hipsRef, 'hipsRef')})}
+                  />
+                </View>
+                <View style={styles.inputWrap}>
+                  <Text style={styles.swithText}>
+                    Hips
+                  </Text>
+                  <InputText
+                    styleInputInner={styles.input}
+                    placeholder="Hips"
+                    value={hips}
+                    onChange={text => setHips(text)}
+                    returnKeyType="done"
+                    keyboardType="numeric"
+                    ref={hipsRef}
+                    name="hipsRef"
+                    onSubmitEditing={calculate}
+                  />
+                </View>
+              </View>
+              {
+                calculateSize ? (
+                  <View style={{ flex: 1 }}>
+                    <Text style={styles.calculateText}>
+                      Your size is:
+                      <Text style={styles.calculateResultTextBold}>
+                        {` ${calculateSize}`}
+                      </Text>
+                    </Text>
+                    <Text style={styles.calculateText}>
+                      Your UK Size is:
+                      <Text style={styles.calculateResultTextBold}>
+                        {` ${sizes.UK.gino[calculateSize]}`}
+                      </Text>
+                    </Text>
+                  </View>
+                ) : null
+              }
+              <Button
+                style={styles.buttonCal}
+                success
+                onPress={calculate}
+              >
+                <Text style={styles.buttonTextCal}>
+                  Calculate
+                </Text>
+              </Button>
+              <TitleText text="Size Charts" />
+            </View>
+          </Fragment>
+        </KeyboardAwareScrollView>
+      </SafeAreaView>
+    </Fragment>
+  );
 }
 
 export default SizeGuide;
