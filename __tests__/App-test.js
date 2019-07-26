76589132580c33cc7594922614c8dc623948b803
Vitaliy Belishov <belishov.v@gmail.com>
2019-07-26 19:38:18 +0300

    jest several data test

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -8,10 +8,67 @@ import Sizes from '../source/size/size-guide.screen.js';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import {
+  sizes,
+  sizesList,
+  collections,
+  collectionsList,
+} from '../source/size/size.const.js';
+
+import {
+  sizesTest,
+  sizesTestNoGino,
+  sizesListTest,
+} from '../source/size/size.const.js';
 
 it('renders correctly', () => {
   const snap = renderer
-    .create(<Sizes />)
+    .create(
+      <Sizes
+        sizes={sizes}
+        sizesList={sizesList}
+        collections={collections}
+        collectionsList={collectionsList}
+      />)
+    .toJSON();
+  expect(snap).toMatchSnapshot();
+});
+
+it('renders no found user size', () => {
+  const snap = renderer
+    .create(
+      <Sizes
+        sizes={sizesTest}
+        sizesList={sizesList}
+        collections={collections}
+        collectionsList={collectionsList}
+      />)
+    .toJSON();
+  expect(snap).toMatchSnapshot();
+});
+
+it('renders no found gino size', () => {
+  const snap = renderer
+    .create(
+      <Sizes
+        sizes={sizesTestNoGino}
+        sizesList={sizesList}
+        collections={collections}
+        collectionsList={collectionsList}
+      />)
+    .toJSON();
+  expect(snap).toMatchSnapshot();
+});
+
+it('renders incorect sizesList', () => {
+  const snap = renderer
+    .create(
+      <Sizes
+        sizes={sizesTest}
+        sizesList={sizesListTest}
+        collections={collections}
+        collectionsList={collectionsList}
+      />)
     .toJSON();
   expect(snap).toMatchSnapshot();
 });
